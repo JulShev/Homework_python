a565c63ece36ad9b3b6346d2154acb50b4c62056
Julia Shevkunova <shevkunova.yulia@gmail.com>
2022-05-30 12:20:28 +0300

    операции со списками и словарями: создание, добавление, удаление элментов списка

---
+++ lesson_2.py
@@ -0,0 +1,205 @@
+# создаем компанию с отделами с сотрудниками
+company = {
+    'dep_management': [
+    {
+        'employee': 'director',
+        'name' : 'Peter',
+        'age' : 27,
+        'salary' : 1500
+    },
+    {
+        'employee' : 'assistant',
+        'name' : 'Mary',
+        'age' : 25,
+        'salary' : 1200
+    },
+    {
+    'employee' : 'secretary',
+    'name' : 'Kate',
+    'age' : 21,
+    'salary' : 800
+    }
+    ],
+    'dep_development': [
+        {
+        'employee' : 'boss',
+        'name' : 'Nikk',
+        'age' : 45,
+        'salary' : 2000
+        },
+        {
+        'employee': 'employee_1',
+        'name': 'Lili',
+        'age': 18,
+        'salary': 500
+        },
+        {
+        'employee': 'employee_2',
+        'name': 'Dima',
+        'age': 20,
+        'salary': 700
+         },
+        {
+        'employee': 'employee_3',
+        'name': 'Fain',
+        'age': 21,
+        'salary': 880
+        },
+        {
+        'employee': 'employee_4',
+        'name': 'Gena',
+        'age': 20,
+        'salary': 900
+        },
+        {
+        'employee': 'employee_5',
+        'name': 'Leon',
+        'age': 21,
+        'salary': 750
+        },
+        {
+        'employee': 'employee_6',
+        'name': 'Marina',
+        'age': 24,
+        'salary': 700
+        }
+    ],
+        'dep_operations': [
+        {
+        'employee': 'boss',
+        'name' : 'Alex',
+        'age' : 35,
+        'salary' : 3000
+        },
+        {
+        'employee' : 'employee_1',
+        'name' : 'Olga',
+        'age' : 33,
+        'salary' : 1100
+        },
+        {
+         'employee' : 'employee_2',
+         'name' : 'Kate',
+         'age' : 21,
+         'salary' : 800
+        },
+        {
+         'employee': 'employee_3',
+         'name': 'Alex',
+         'age': 35,
+        'salary': 3000
+        },
+        {
+        'employee': 'employee_4',
+        'name': 'Lili',
+        'age': 18,
+        'salary': 500
+        },
+        {
+        'employee': 'employee_5',
+        'name': 'Dima',
+        'age': 20,
+        'salary': 700
+         }
+    ],
+        'dep_testing': [
+        {
+        'employee': 'boss',
+        'name': 'Jon',
+        'age': 21,
+        'salary': 880
+        },
+        {
+         'employee': 'employee_1',
+         'name': 'Misha',
+         'age': 35,
+        'salary': 3000
+        },
+        {
+        'employee': 'employee_2',
+        'name': 'Lili',
+        'age': 18,
+        'salary': 500
+        },
+        {
+        'employee': 'employee_3',
+        'name': 'Dima',
+        'age': 20,
+        'salary': 700
+         },
+        {
+         'employee': 'employee_3',
+         'name': 'Alex',
+         'age': 35,
+        'salary': 3000
+        },
+        {
+        'employee': 'employee_4',
+        'name': 'Gleb',
+        'age': 18,
+        'salary': 500
+        }
+    ],
+         'dep_sales': [
+        {
+        'employee': 'boss',
+        'name': 'Dima',
+        'age': 20,
+        'salary': 1900
+         },
+        {
+         'employee': 'employee_1',
+         'name': 'Alex',
+         'age': 35,
+        'salary': 3000
+        },
+        {
+        'employee': 'employee_2',
+        'name': 'Gleb',
+        'age': 18,
+        'salary': 500
+        },
+        {
+        'employee': 'employee_3',
+        'name': 'Dima',
+        'age': 20,
+        'salary': 700
+         },
+        {
+         'employee': 'employee_4',
+         'name': 'Egor',
+         'age': 35,
+        'salary': 3000
+        },
+        {
+        'employee': 'employee_5',
+        'name': 'Nikita',
+        'age': 18,
+        'salary': 500
+        }
+    ]
+}
+
+company.append('dep_marketing')
+
+dep_marketing = {'employee': 'boss', 'name': 'Nikita',  'age': 18, 'salary': 500}, {'employee': 'employee1', 'name': 'Ben',  'age': 18, 'salary': 770},
+dep_management = {'employee': 'boss', 'name': 'Nikita',  'age': 18, 'salary': 500}
+dep_development = {'employee': 'boss', 'name': 'Nikita',  'age': 18, 'salary': 500}
+dep_operations = {'employee': 'boss', 'name': 'Nikita',  'age': 18, 'salary': 500}
+dep_testing = {'employee': 'boss', 'name': 'Nikita',  'age': 18, 'salary': 500}
+dep_sales = {'employee': 'boss', 'name': 'Nikita',  'age': 18, 'salary': 500}
+
+del company[0]
+
+
+
+
+
+
+
+
+
+
+
+
+
